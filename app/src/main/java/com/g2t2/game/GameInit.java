f9b1835f77fa579b9f4a1c3df2707f2580ccf3cd
YeoBenShin <orang3cats@gmail.com>
2025-03-04 00:29:40 +0800

    Created the ability for players to insert the number of players + the initialisation of the players and the parade

--- app/src/main/java/com/g2t2/game/GameInit.java
+++ app/src/main/java/com/g2t2/game/GameInit.java
@@ -1,13 +1,20 @@
 package com.g2t2.game;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import com.g2t2.util.StateFlags;
 import com.g2t2.util.Utility;
+import com.g2t2.types.*;
 
 /**
  * Class handling all game initialization methods
  * @author Oliver
  */
 public class GameInit {
+
+    private static int numOfPlayers;
+
     /**
      * Display the game options in either console or GUI
      * (depending on what isGUIMode in StateFlags is)
@@ -20,6 +27,23 @@ public class GameInit {
             // Console configuration
             Utility.clearConsoleScreen();
             displayConsoleGameOptions();
+            giveOutTheCards();
+
+            // // Test
+            // System.out.println("--TEST--");
+            // for (Player p : ParadeBoard.getPlayers()) {
+            //     System.out.println(p.getName());
+            //     for (Card c : p.getCardsOnHand()) {
+            //         System.out.println("Colour = " + c.getColour() + "| Value = " + c.getValue());
+            //     }
+            //     System.out.println();
+            // }
+
+            // System.out.println("--PARADE BOARD--");
+            // for (Card c : ParadeBoard.getParade()) {
+            //     System.out.println("Colour = " + c.getColour() + "| Value = " + c.getValue());
+            // }
+
         }
     }
 
@@ -29,6 +53,61 @@ public class GameInit {
      */
     private static void displayConsoleGameOptions() {
         // Allow user to configure players, difficulty?
-        
+
+        // - ben
+        Scanner sc = new Scanner(System.in);
+        numOfPlayers = 0;
+
+        // For local option
+
+        // Number of Players
+        while (true) {
+            try {
+                System.out.println("How many players do you want to play with?");
+                System.out.print("Enter the number of players (2 to 6): ");
+                numOfPlayers = Integer.parseInt(sc.nextLine());
+                if (numOfPlayers < 2 || numOfPlayers > 6) {
+                    throw new IllegalArgumentException();
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("A minimum of 2 players and a maximum of 6 players are allowed. Please enter a number between 2 to 6.");
+                System.out.println();
+            }
+        }
+    }
+
+    /**
+     * Method initialises players + parade
+     * @author ben
+     */
+    private static void giveOutTheCards() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Player> players = new ArrayList<>();
+
+        for (int i = 1; i <= numOfPlayers; i++) {
+            while (true) {
+                try {
+                    System.out.print("Enter the name of player " + i + ": ");
+                    String name = sc.nextLine();
+                    if (name.equals("")) {
+                        throw new IllegalArgumentException("Please enter an appropriate name.");
+                    }
+                    players.add(new Player(name, ParadeBoard.getDECK()));
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println();
+                }
+            }
+        }
+        ParadeBoard.setPlayers(players);
+
+        // Setting up the board
+        ArrayList<Card> board = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            board.add(ParadeBoard.getDECK().drawCard());
+        }
+        ParadeBoard.setParade(board);
     }
 }
